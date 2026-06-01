7fa79a5f280f4868a43fd767e6f7d70f29a1a5bb
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-01 12:49:25 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/dev/atharvakulkarni/e_commerce/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dev/atharvakulkarni/e_commerce/ExampleInstrumentedTest.java
@@ -1,4 +1,5 @@
 package dev.atharvakulkarni.e_commerce;
+
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
