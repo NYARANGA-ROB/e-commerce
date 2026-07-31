de1c4d80e836a26280f52ee29a334b737ae8c155
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-01 01:15:44 +0300

    Fix formatting and whitespace in ExampleUnitTest

--- app/src/test/java/dev/atharvakulkarni/e_commerce/ExampleUnitTest.java
+++ app/src/test/java/dev/atharvakulkarni/e_commerce/ExampleUnitTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    
     @Test
     public void addition_isCorrect() {
         
