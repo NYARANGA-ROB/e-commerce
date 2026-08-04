a8d490d3f0b3a7a5b07b9573f73292f295e137e2
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-04 08:44:54 +0300

    Format ExampleInstrumentedTest.java

--- app/src/androidTest/java/dev/atharvakulkarni/e_commerce/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dev/atharvakulkarni/e_commerce/ExampleInstrumentedTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
     
 public class ExampleInstrumentedTest {
+    
     @Test
     public void useAppContext() {
         // Context of the app under test.
