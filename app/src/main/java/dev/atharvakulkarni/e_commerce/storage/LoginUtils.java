47b678a868f2aa92f25b0a6990318c9f771f65e5
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-08-11 17:18:52 +0300

    Update LoginUtils.java

--- app/src/main/java/dev/atharvakulkarni/e_commerce/storage/LoginUtils.java
+++ app/src/main/java/dev/atharvakulkarni/e_commerce/storage/LoginUtils.java
@@ -6,7 +6,6 @@ import dev.atharvakulkarni.e_commerce.view.User;
 
 public class LoginUtils {
     private static final String SHARED_PREF_NAME = "shared_preference";
-
     private static LoginUtils mInstance;
     private Context mCtx;
 
